582cd73986911a79e636fec70d9c70a1164581b4
NamKyeongho <sktmzp9652@naver.com>
2017-09-25 15:07:30 +0900

    17.09.25

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/public', function () {
-    return view('welcome');
+Route::get('hello', function () {
+    return 'hellow';
 });
